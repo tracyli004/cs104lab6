fe0944db8705395493dfbaa1a298be1fa6125f78
Tracy Li <tracyli@Tracys-MacBook-Pro-2.local>
2025-02-22 23:25:42 -0800

    added mathfuncs.cpp

---
+++ mathfuncs.cpp
@@ -0,0 +1,21 @@
+#include "mathfuncs.h"
+#include <stdexcept>
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b == 0) {
+        throw std::runtime_error("Error: Division by zero");
+    }
+    return a / b;
+}
