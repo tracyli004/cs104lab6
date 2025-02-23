af43c1eca99b4933033e618fc0abc805825f66ae
Tracy Li <tracyli@Tracys-MacBook-Pro-2.local>
2025-02-22 23:29:32 -0800

    added help info main

--- scaffold.cpp
+++ scaffold.cpp
@@ -3,10 +3,22 @@
 
 using namespace std;
 
+void printHelp() {
+    cout << "help documentation\n"
+         << "  add <num1> <num2>   - Adds two numbers\n"
+         << "  sub <num1> <num2>   - Subtracts two numbers\n"
+         << "  mul <num1> <num2>   - Multiplies two numbers\n"
+         << "  div <num1> <num2>   - Divides two numbers\n"
+         << "  quit                - Exit the program\n";
+}
+
 void processCommand(const string &command) {
     double num1, num2;
     
-    if (command == "add" || command == "sub" || command == "mul" || command == "div") {
+    if (command == "help") {
+        printHelp();
+    } 
+    else if (command == "add" || command == "sub" || command == "mul" || command == "div") {
         if (!(cin >> num1 >> num2)) {
             cout << "Invalid input. Please enter two numbers." << endl;
             cin.clear();
